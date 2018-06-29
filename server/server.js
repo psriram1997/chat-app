5eda894446b67ed6717fed782361d7515b473e76
sriram perla <psriram1997@gmail.com>
2018-06-29 17:03:36 +0530

    connect and disconnect users

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const path = require('path');
 const express = require('express');
 const app     = express();
+const http    = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+    console.log('User connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected')
+    });
+});
 
-app.listen(port,() => {
+server.listen(port,() => {
 console.log(`server started on port ${port}`);
 });
