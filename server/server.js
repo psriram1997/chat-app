211232465157b582a8ce0f066fd909cf40dfcb71
sriram perla <psriram1997@gmail.com>
2018-06-29 16:20:36 +0530

    initial setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app     = express();
+
+const publicPath = path.join(__dirname,'../public');
+console.log(publicPath);
+
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000,() => {
+console.log('Server started');
+});
